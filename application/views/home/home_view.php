48490c0baecaefe74dbf46af139bd13a44902ae3
allii98 <alihore042@gmail.com>
2022-01-14 13:44:26 +0700

    Perbaikan

--- application/views/home/home_view.php
+++ application/views/home/home_view.php
@@ -55,7 +55,26 @@ if ($this->session->userdata('sess_level') == 1) {
           ?></b></h3>
       <div class="row-fluid">
         <span class="alert alert-info">Selamat Datang di <b>MIPS - <?php echo $namamodul['nama']; ?> - <?php echo $namapt['nama']; ?> - <?= $this->session->userdata('sess_nama_pt') ?></b></span>
+        <!-- <input type="text" name="" id="" value="<?= $this->session->userdata('sess_level') ?>"> -->
+        <br>
+        <br>
+        <?php if ($this->session->userdata('sess_level') == 3) { ?>
 
+          <div class="span12">
+            <ul class="dshb_icoNav tac">
+              <li><a href="javascript:void(0)" style="background-image: url(img/gCons/multi-agents.png)"><span class="label label-info">+10</span> Users</a></li>
+              <li><a href="javascript:void(0)" style="background-image: url(img/gCons/world.png)">Map</a></li>
+              <li><a href="javascript:void(0)" style="background-image: url(img/gCons/configuration.png)">Settings</a></li>
+              <li><a href="javascript:void(0)" style="background-image: url(img/gCons/lab.png)">Lab</a>
+              </li>
+              <li><a href="javascript:void(0)" style="background-image: url(img/gCons/van.png)"><span class="label label-success">$2851</span> Delivery</a></li>
+              <li><a href="javascript:void(0)" style="background-image: url(img/gCons/pie-chart.png)">Charts</a></li>
+              <li><a href="javascript:void(0)" style="background-image: url(img/gCons/edit.png)">Add New Article</a></li>
+              <li><a href="javascript:void(0)" style="background-image: url(img/gCons/add-item.png)"> Add New Page</a></li>
+              <li><a href="javascript:void(0)" style="background-image: url(img/gCons/chat-.png)"><span class="label label-important">26</span> Comments</a></li>
+            </ul>
+          </div>
+        <?php } ?>
         <!--<br>
             <br>
 
--- application/views/main/main_menu_sidebar_view.php
+++ application/views/main/main_menu_sidebar_view.php
@@ -1,71 +1,73 @@
 <a href="javascript:void(0)" class="sidebar_switch on_switch ttip_r" title="Hide Sidebar">Sidebar switch</a>
-            <div class="sidebar">
-        
-        <div class="antiScroll">
-          <div class="antiscroll-inner">
-            <div class="antiscroll-content">
-          
-              <div class="sidebar_inner">
-                <form action="index.php?uid=1&amp;page=search_page" class="input-append" method="post" >
-                  <input autocomplete="off" name="query" class="search_query input-medium" size="16" type="text" placeholder="Search..." /><button type="submit" class="btn"><i class="icon-search"></i></button>
-                </form>
-                <div id="side_accordion" class="accordion">
-                  
-                  <div class="accordion-group">
-                    <div class="accordion-heading">
-                      <a href="#collapseThree" data-parent="#side_accordion" data-toggle="collapse" class="accordion-toggle">
-                        <i class="icon-user"></i> Pengguna
-                      </a>
-                    </div>
-                    <div class="accordion-body collapse" id="collapseThree">
-                      <div class="accordion-inner">
-                        <ul class="nav nav-list">
-                          <li><a href="javascript:void(0)" onclick="getcontents('users','<?php echo $this->session->userdata('sess_token');?>');">Data Pengguna</a></li>
-                        </ul>
-                        
-                      </div>
-                    </div>
+<div class="sidebar">
+
+  <div class="antiScroll">
+    <div class="antiscroll-inner">
+      <div class="antiscroll-content">
+
+        <div class="sidebar_inner">
+          <form action="index.php?uid=1&amp;page=search_page" class="input-append" method="post">
+            <!-- <input autocomplete="off" name="query" class="search_query input-medium" size="16" type="text" placeholder="Search..." /><button type="submit" class="btn"><i class="icon-search"></i></button> -->
+          </form>
+          <div id="side_accordion" class="accordion">
+
+            <?php
+            foreach ($modules as $mod) {
+              if ($mod['position'] == 1 and $mod['have_child'] == 'Y') {
+            ?>
+
+                <div class="accordion-group">
+                  <div class="accordion-heading">
+                    <a href="#<?php echo $mod['name']; ?>" data-parent="#side_accordion" data-toggle="collapse" class="accordion-toggle">
+                      <i class="icon-th"></i> <?php echo $mod['name']; ?>
+                    </a>
                   </div>
-                  <div class="accordion-group">
-                    <div class="accordion-heading">
-                      <a href="#collapseFour" data-parent="#side_accordion" data-toggle="collapse" class="accordion-toggle">
-                        <i class="icon-cog"></i> Pengaturan
-                      </a>
-                    </div>
-                    <div class="accordion-body collapse" id="collapseFour">
-                      <div class="accordion-inner">
-                        <ul class="nav nav-list">
-                          <li class="nav-header">Menu dan Role</li>
-                          <li><a href="javascript:void(0)" onclick="getcontents('module','<?php echo $this->session->userdata('sess_token');?>');">Module</a></li>
-                          <li><a href="javascript:void(0)" onclick="getcontents('role/index','<?php echo $this->session->userdata('sess_token');?>');">Role</a></li>
-                        </ul>
-                      </div>
+                  <div class="accordion-body collapse" id="<?php echo $mod['name']; ?>">
+                    <div class="accordion-inner">
+                      <ul class="nav nav-list">
+                        <li class="nav-header"><?php echo $mod['name']; ?></li>
+                        <?php
+                        foreach ($modules as $d) {
+                          if ($d['position'] == 2 and $d['have_child'] == 'Y' and $d['parent'] == $mod['id']) {
+
+                            echo '<li class="dropdown">
+                                    <a class="line_menu" href="javascript:void(0)" onclick=getcontents("' . $d['controller'] . '","' . $this->session->userdata('sess_token') . '")>' . $d['name'] . '<b class="caret-right"></b></a>
+                                        <ul class="dropdown-menu">';
+                            foreach ($modules as $ex) {
+                              if ($ex['position'] == 3 and $ex['parent'] == $d['id']) {
+                                echo '<li><a href="javascript:void(0)" onclick=getcontents("' . $ex['controller'] . '","' . $this->session->userdata('sess_token') . '");><span class="fa fa-dot-circle-o"></span> ' . $ex['name'] . '</a></li>';
+                              }
+                            }
+                            echo '</ul>
+                          </li>';
+                          } else if ($d['position'] == 2 and $d['have_child'] == 'N' and $d['parent'] == $mod['id']) {
+
+                            if ($d['line'] == 1) {
+                              //ini bedasnya ditambah class line_menu
+                              echo '<li><a class="line_menu" href="javascript:void(0)" onclick=getcontents("' . $d['controller'] . '","' . $this->session->userdata('sess_token') . '")>' . $d['name'] . '</a></li>';
+                            } else {
+                              echo '<li><a href="javascript:void(0)" onclick=getcontents("' . $d['controller'] . '","' . $this->session->userdata('sess_token') . '")>' . $d['name'] . '</a></li>';
+                            }
+                          }
+                        }
+                        ?>
+                      </ul>
                     </div>
                   </div>
                 </div>
-                
-                <div class="push"></div>
-              </div>
-                 
-              <div class="sidebar_info">
-                <ul class="unstyled">
-                  <li>
-                    <span class="act act-warning">65</span>
-                    <strong>New comments</strong>
-                  </li>
-                  <li>
-                    <span class="act act-success">10</span>
-                    <strong>New articles</strong>
-                  </li>
-                  <li>
-                    <span class="act act-danger">85</span>
-                    <strong>New registrations</strong>
-                  </li>
-                </ul>
-              </div> 
-            
-            </div>
+
+            <?php }
+            } ?>
+
           </div>
+
+          <div class="push"></div>
         </div>
-      
+
+
+
       </div>
+    </div>
+  </div>
+
+</div>
